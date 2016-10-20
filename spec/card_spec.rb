56a8e30785bd645b58b00181a6370045427f6aad
Martin Hsia <marandy0905@gmail.com>
2016-10-20 18:40:00 +0800

    Create card_spec, Fix GemFile Rubocop.yml

---
+++ spec/card_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'card specifications' do
+  before do
+    @search_result = Spotify::Search.new('Eyes Shut')
+    @track_id = '2xzzSkFGtf0w5ZATRYa1Tg'
+  end
+
+  it 'should be able to get a hash from Search Class' do
+    @search_result.get_songs.is_a?(Hash)
+  end
+
+  it 'should be able to get a track_id' do
+    @search_result.get_songs[@track_id].track_id = @track_id
+  end
+end
