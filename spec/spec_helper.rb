d4e2ecc8bc6e2ba7f470401c1057cf0f6abaf006
Martin Hsia <marandy0905@gmail.com>
2016-10-21 16:47:32 +0800

    VCR for test included

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,9 @@ require 'webmock'
 
 require_relative '../lib/search.rb'
 
-#FIXTURES_FOLDER = 'spec/fixtures'
-#CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes"
-#CASSETTE_FILE = 'spotify_api'
+FIXTURES_FOLDER = 'spec/fixtures'
+CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes"
+CASSETTE_FILE = 'spotify_api'
 #CREDENTIALS = YAML.load(File.read('../config/credentials.yml'))
 #RESULT_FILE = "#{FIXTURES_FOLDER}/results.yml"
 #SPOTIFY_RESULT = YAML.load(File.read(RESULT_FILE))
